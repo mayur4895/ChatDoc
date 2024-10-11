903375ef7e3b13e54f6f6584422f37cd24973b2b
mayur shinde <kssmsslss330@gmail.com>
2024-10-11 14:42:23 +0530

    "done todyas"

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -57,7 +57,7 @@ export const ourFileRouter = {
 
          
       } catch (err) {
-       
+            console.log(err)
           await db.file.update({
             data: { uploadStatus: "FAILED" },
             where: { 
